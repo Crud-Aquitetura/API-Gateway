86949556ed560c667a140c59f430bb7f9f72c581
duds09http <rodriguesmarqueseduardo09@gmail.com>
2025-06-10 16:44:47 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { insertController } from '../controllers/insertController';
 import { updateController } from '../controllers/updateController';
 import { deleteController } from '../controllers/deleteController';
 import { loggerMiddleware } from '../middlewares/logger';
+import { insertLocalController, selectLocaisController } from '../controllers/locaisController';
 
 const router = Router();
 
@@ -11,5 +12,7 @@ router.get('/', loggerMiddleware, selectController); // Rota para listar eventos
 router.post('/', loggerMiddleware, insertController); // Rota para inserir eventos
 router.put('/:id', loggerMiddleware, updateController); // Rota para atualizar eventos por ID
 router.delete('/:id', loggerMiddleware, deleteController); // Rota para deletar eventos por ID
+router.post('/locais', loggerMiddleware, insertLocalController); // Endpoint para cadastrar local
+router.get('/locais', loggerMiddleware, selectLocaisController); // Endpoint para listar locais
 
 export default router;

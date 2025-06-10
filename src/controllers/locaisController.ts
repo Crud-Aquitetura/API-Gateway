12a375396d2240856db58a24cbc485410d041ad4
duds09http <rodriguesmarqueseduardo09@gmail.com>
2025-06-10 16:45:54 -0300

    Add files via upload

---
+++ src/controllers/locaisController.ts
@@ -0,0 +1,32 @@
+
+import { Request, Response, NextFunction } from 'express';
+import { getAvailableInstance } from '../services/healthChecker';
+import axios from 'axios';
+
+export async function insertLocalController(req: Request, res: Response, next?: NextFunction) {
+  const targetUrl = getAvailableInstance('locais'); // Usa o serviço 'locais'
+  if (!targetUrl) {
+    return next && next({ status: 503, message: 'Sem instância de locais disponível' });
+  }
+  try {
+    const response = await axios.post(targetUrl, req.body); // Encaminha o POST
+    res.status(response.status).json(response.data);
+  } catch (error: any) {
+    if (next) return next(error);
+    throw error;
+  }
+}
+
+export async function selectLocaisController(req: Request, res: Response, next?: NextFunction) {
+    const targetUrl = getAvailableInstance('locais');
+    if (!targetUrl) {
+        return next && next({ status: 503, message: 'Sem instância de locais disponível' });
+    }
+    try {
+        const response = await axios.get(targetUrl); // Encaminha o GET
+        res.status(response.status).json(response.data);
+    } catch (error: any) {
+        if (next) return next(error);
+        throw error;
+    }
+}

aea35ce2a2af64a0b8df4838308ef5f8fea71dae
Francisco Leonardo <148999121+ingres-dev@users.noreply.github.com>
2025-06-18 10:20:13 -0300

    Update locaisController.ts

--- src/controllers/locaisController.ts
+++ src/controllers/locaisController.ts
@@ -3,10 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 
 export async function insertLocalController(req: Request, res: Response, next?: NextFunction) {
-  const targetUrl = getAvailableInstance('locais'); // Usa o serviço 'locais'
-  if (!targetUrl) {
-    return next && next({ status: 503, message: 'Sem instância de locais disponível' });
-  }
+
   try {
     const response = await axios.post(targetUrl, req.body); // Encaminha o POST
     res.status(response.status).json(response.data);
